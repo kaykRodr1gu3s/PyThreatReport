4abca93d518682aaf3b08a052f2e9ba339a51ce5
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2024-05-26 19:34:27 -0300

    Add the hash file

--- Tools/serpro_ip.py
+++ Tools/serpro_ip.py
@@ -5,6 +5,7 @@ class serpro_ip_tracker:
     This class will collect all ips on https://s3.i02.estaleiro.serpro.gov.br/blocklist/blocklist.txt
     
     """
+    
     def ip_tracker(self) -> list:
         """
         This folder will return a list of ips 
