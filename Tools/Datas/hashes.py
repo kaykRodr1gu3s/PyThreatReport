cee3df3c15e5aafef08abd488ec30f3f9511a15a
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2024-06-04 16:05:30 -0300

    some settings
    add a summary of the class and ther method

--- Tools/Datas/hashes.py
+++ Tools/Datas/hashes.py
@@ -1,4 +1,7 @@
 class Hashes_class:
+    """This class will iterate to full-hash-md5-aa.txt file, and return a list of file.
+    """
+
     def __init__(self, file):
         self.file = open(file)
 
@@ -18,10 +21,12 @@ class Hashes_class:
         
 
 def hashes_function():
-
+    """
+    This function will return the quantity selected of ips
+    """
     hash_list = []
     for line in Hashes_class("Tools\\Datas\\full-hash-md5-aa.txt"):
-        if len(hash_list) == 10:
+        if len(hash_list) == 25:
             return hash_list
             
         else:
