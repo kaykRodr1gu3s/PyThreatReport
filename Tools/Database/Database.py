b681898059b79afe59f04742e6605f576f2eb36c
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2024-06-20 17:00:36 -0300

    Update Database.py

--- Tools/Database/Database.py
+++ Tools/Database/Database.py
@@ -10,21 +10,27 @@ class Ips_Db:
 
     @property
     def create_table(self):
-            """This method will create the database"""
-            try:
-                self.cursor.execute("CREATE TABLE Ips_tables(Ip TEXT UNIQUE, Abuse_Confidence_Score TEXT, Date DATE)")
-                self.conection.commit()
+        """
+        This method will create the database
+        """
+        try:
+            self.cursor.execute("CREATE TABLE Ips_tables(Ip TEXT UNIQUE, Abuse_Confidence_Score TEXT, Date DATE)")
+            self.conection.commit()
 
-            except Exception as Table_aready_exist:
-                print(Table_aready_exist)
+        except Exception as Table_aready_exist:
+            print(Table_aready_exist)
 
     def inserting_value(self, data: list[tuple]):
-        """This method will insert the data to the database"""
+        """
+        This method will insert the data to the database
+        """
         self.cursor.executemany("INSERT INTO Ips_tables(Ip, Abuse_Confidence_Score, Date) VALUES (?,?,?)", data)
         self.conection.commit()            
 
     def select_data(self):
-        """This method will return all ip from the databse"""
+        """
+        This method will return all ip from the databse
+        """
         return self.cursor.execute("SELECT Ip FROM Ips_tables").fetchall()
     
 ip = Ips_Db()
