9e843ebfef3a8ddf95418f624411f8de80f2a9ee
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2024-06-20 16:13:07 -0300

    Add the directory to DB

--- Tools/Database/Database.py
+++ Tools/Database/Database.py
@@ -1,20 +1,16 @@
 import sqlite3
 from pathlib import Path
-from datetime import datetime
-import pytz
 
 class Ips_Db:
-    def __init__(self,option: str):
-            
-            self.conection = sqlite3.connect("ips_hashes.sqlite3")
+    def __init__(self):
+            ROOT_PATH = Path(__file__).parent
+            self.conection = sqlite3.connect(ROOT_PATH / "ips.sqlite3")
             self.cursor = self.conection.cursor()
             self.cursor.row_factory = sqlite3.Row
-            self.option = option
 
     @property
     def create_table(self):
-        if self.option == "ips":
-
+            """This method will create the database"""
             try:
                 self.cursor.execute("CREATE TABLE Ips_tables(Ip TEXT UNIQUE, Abuse_Confidence_Score TEXT, Date DATE)")
                 self.conection.commit()
@@ -22,37 +18,14 @@ class Ips_Db:
             except Exception as Table_aready_exist:
                 print(Table_aready_exist)
 
-        elif self.option == "hash":
-            try:
-                self.cursor.execute("CREATE TABLE Hash_table(Hash TEXT UNIQUE, Veredict TEXT, Date DATE)")
-                print("tabela criada")
-                self.conection.commit()
-            except Exception as Table_aready_exist:
-                print(Table_aready_exist)
-
     def inserting_value(self, data: list[tuple]):
-        if self.option == "ips":
-            self.cursor.executemany("INSERT INTO Ips_tables(Ip, Abuse_Confidence_Score, Date) VALUES (?,?,?)", data)
-            self.conection.commit()      
-        else:
-            self.cursor.executemany("INSERT INTO Hash_table(Ip, Abuse_Confidence_Score, Date) VALUES (?,?,?)", data)
-            self.conection.commit()      
+        """This method will insert the data to the database"""
+        self.cursor.executemany("INSERT INTO Ips_tables(Ip, Abuse_Confidence_Score, Date) VALUES (?,?,?)", data)
+        self.conection.commit()            
 
     def select_data(self):
-        if self.option == "ips":
-           return self.cursor.execute("SELECT Ip FROM Ips_tables").fetchall()
-        else:
-            return self.cursor.execute("SELECT Ip FROM Ips_tables").fetchall()
+        """This method will return all ip from the databse"""
+        return self.cursor.execute("SELECT Ip FROM Ips_tables").fetchall()
     
-
-class Ip(Ips_Db):
-    pass
-
-class Hash(Ips_Db):
-    pass
-
-ip = Ip("ips")
-hashes = Hash("hash")
-
+ip = Ips_Db()
 ip.create_table
-hashes.create_table
